7ce2a5a164a450b082d61c2d8ace8acc413478a9
Eloy Perez <ep.falagan@gmail.com>
2024-04-18 10:52:50 +0200

    define user domain

---
+++ internal/user.go
@@ -0,0 +1,42 @@
+package gohtmx
+
+import (
+	"context"
+	"time"
+)
+
+type User struct {
+	Id        string
+	Name      string
+	Surname   string
+	Email     string
+	Active    bool
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type UserUpdate struct {
+	Name    *string
+	Surname *string
+	Email   *string
+	Active  *bool
+}
+
+type UserFilters struct {
+	Id      *string
+	Name    *string
+	Surname *string
+	Email   *string
+	Active  *bool
+	// Restrict to subset of results.
+	Offset int
+	Limit  int
+}
+
+type UserService interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+	FindUserById(ctx context.Context, id string) (*User, error)
+	FindUsers(ctx context.Context, filters UserFilters) ([]*User, error)
+	UpdateUser(ctx context.Context, user UserUpdate) (*User, error)
+	DeleteUser(ctx context.Context, id string) error
+}
